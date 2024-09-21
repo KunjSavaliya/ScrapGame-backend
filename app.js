189b5e5c3de2ecc2ac5bb4b6c711f10706fab0b3
KunjSavaliya <kunj.bvminfotech@gmail.com>
2024-09-21 12:52:55 +0530

    Initial commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware to enable CORS
 app.use(cors({
-    origin: 'http://localhost:3000', // Change this to match your frontend URL
+    origin: 'http://localhost:3011', // Change this to match your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add other methods as needed
     credentials: true // Allow credentials if required
 }));
@@ -26,7 +26,7 @@ app.use(express.json());
 // Routes
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3012;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
